6111bfeb9b6ee1fd240f784a484948b59b7ed76c
0xC00005 <0xc00005@gmail.com>
2023-05-29 12:06:12 -0400

    more print

--- print_mat_structure.py
+++ print_mat_structure.py
@@ -1,26 +1,25 @@
 import scipy.io
 import numpy as np
 
-
-def print_mat_file(filename):
+def print_mat_file(filename, level=0):
     # Load .mat file
     mat = scipy.io.loadmat(filename)
 
-    # Print top-level structure
-    for key, value in mat.items():
-        if key in ["__header__", "__version__", "__globals__"]:
-            print(f"{key}: {value}")
+    def print_item(key, item, level=0):
+        # Print indentation
+        print("  " * level + f"{key}: ", end="")
+        if isinstance(item, np.ndarray) and item.size > 0:
+            if item.dtype == 'object':
+                print(f"type({type(item)}), shape({item.shape})")
+                for i, subitem in np.ndenumerate(item):
+                    print_item(i, subitem, level + 1)
+            else:
+                print(f"type({type(item)}), value({item})")
         else:
-            print(f"{key}: type({type(value)}), shape({value.shape})")
+            print(f"{item}")
 
-            # If value is numpy ndarray and contains dtype object, it can have nested structures.
-            if isinstance(value, np.ndarray) and value.dtype == 'object':
-                for i, item in np.ndenumerate(value):
-                    print(f"  {i}:", )
-                    if isinstance(item, np.ndarray) and item.size > 0:
-                        print(f"    type({type(item[0])}), value({item[0]})")
-                    else:
-                        print(f"    {item}")
-
+    # Print structure and data
+    for key, value in mat.items():
+        print_item(key, value, level)
 
 print_mat_file('input_graphs/SuiteSparse Matrix Collection/dwt_307.mat')
