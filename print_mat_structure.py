75ca29cdb26c27199f35358e020f8ddfea47ff0b
0xC00005 <0xc00005@gmail.com>
2023-05-29 12:02:28 -0400

    update print mat infio

--- print_mat_structure.py
+++ print_mat_structure.py
@@ -1,8 +1,25 @@
 import scipy.io
 
-# Replace 'file.mat' with your .mat file path
-mat_file = scipy.io.loadmat('input_graphs/SuiteSparse Matrix Collection/dwt_307.mat')
 
-# print out all keys/values to view the structure of the file
-for key in mat_file:
-    print(f"{key}: {mat_file[key]}")
+def print_mat_file(filename):
+    # Load .mat file
+    mat = scipy.io.loadmat(filename)
+
+    # Print top-level structure
+    for key, value in mat.items():
+        if key in ["__header__", "__version__", "__globals__"]:
+            print(f"{key}: {value}")
+        else:
+            print(f"{key}: type({type(value)}), shape({value.shape})")
+
+            # If value is numpy ndarray and contains dtype object, it can have nested structures.
+            if isinstance(value, np.ndarray) and value.dtype == 'object':
+                for i, item in np.ndenumerate(value):
+                    print(f"  {i}:", )
+                    if isinstance(item, np.ndarray) and item.size > 0:
+                        print(f"    type({type(item[0])}), value({item[0]})")
+                    else:
+                        print(f"    {item}")
+
+
+print_mat_file('your_mat_file.mat')
